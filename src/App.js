d7827bf40cbf1be7993b9a13aea6b4e0bddcd87e
Mario Carbache <mariocarbache@gmail.com>
2023-08-15 15:49:04 -0400

    Api added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, {Component} from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Navbar from "./Navbar";
+import WeatherNY from "./NewYork";
 
 function App(){
   return (
     <Router>
       <div>
         <Navbar />
+        <WeatherNY />
       </div>
       <Routes>
-
+        
       </Routes>
     </Router>
   );
--- src/NewYork.js
+++ src/NewYork.js
@@ -1,4 +1,43 @@
-import React from "react";
+import React, {useEffect, useState}from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import './style.css';
+
+
+function WeatherNY(){
+
+    const [NYweather, setNYweather] = useState([]);
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            try{
+                const response = await axios.get("http://api.openweathermap.org/data/2.5/forecast?lat=40.7143&lon=-74.006&appid=c06ce40078fb8ad660220485fcca1328");
+                console.log(response.data);
+                setNYweather(response.data);
+                console.log(NYweather);
+            }catch(error){
+                console.error("Error fetching data", error);
+            }
+        };
+
+        fetchWeather();
+
+    }, []);
+
+    return(
+        <div>
+            <h1>New York City</h1>
+            <ul>
+                {/* {NYweather.map((city) =>(
+                    <li key={city.dt}>
+                        <p>Current Weather: {city.weather[0].description}</p>
+                    </li>
+                ))} */}
+            </ul>
+        </div>
+    );
+}
+
+export default WeatherNY;
+
 
